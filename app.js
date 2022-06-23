72628ba6b34452bea60384a0ca2e00710110ec30
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-06-23 17:03:09 +0530

    partials include + date module create + about page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,19 +9,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = ["Brush your teeth", "Eat breakfast", "Get to work"];
+const workItems = []
 
 app.get("/", function (_req, res) {
-    let today = new Date();
-    let options = { weekday: 'long', month: 'long', day: 'numeric' };
-    let day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
     res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
     let new_item = req.body.next_task;
-    items.push(new_item);
-    res.redirect("/");
-})
+    if (req.body.list === "Work") {
+        workItems.push(new_item);
+        res.redirect("/work");
+    } else {
+        items.push(new_item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
